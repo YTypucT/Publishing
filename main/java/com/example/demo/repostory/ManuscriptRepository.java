59ccd502822caae72577c474cec9c6c0f4bf2937
Zhmurenko Viktor <61802122+YTypucT@users.noreply.github.com>
2022-02-09 20:56:55 +0300

    Delete main directory

--- main/java/com/example/demo/repostory/ManuscriptRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repostory;
-
-import com.example.demo.entity.Manuscript;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ManuscriptRepository extends JpaRepository<Manuscript, Integer> {
-}
--- main/java/com/example/demo/repostory/WriterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repostory;
-
-import com.example.demo.entity.Writer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WriterRepository extends JpaRepository<Writer, Integer> {
-}
